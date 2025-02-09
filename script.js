c41c84fab7d6fcea4d8e421b36e1a86db658b30e
mariob0503 <mario@simplix.solutions>
2025-02-09 12:10:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // --------------------------------------------------------------------------------
-// Global variables for scene, camera, renderer, controls, and simulation objects.
+// Global variables for scene, camera, renderer, controls, and simulation objects..
 // --------------------------------------------------------------------------------
 let scene, camera, renderer, controls, composer;
 let particleSystem, particlePositions, particleVelocities;
